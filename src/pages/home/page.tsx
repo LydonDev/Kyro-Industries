31b677b246e9af71df3ade9a28bf5f092e115033
Fraser <lydon@frasers-mbp.home>
2025-05-07 17:43:01 +0100

    darkmode

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -3,18 +3,26 @@ import logo from '../../../src/assets/logo.png';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center px-4 py-12 relative">
+    <div className="flex flex-col items-center justify-center px-4 py-12 relative min-h-screen w-full">
       <div className="absolute top-4 right-4">
-        <img src={logo} alt="Kyro Logo" className="h-12 w-auto" />
+        <img 
+          src={logo} 
+          alt="Kyro Logo" 
+          className="h-12 w-auto invert filter" 
+        />
       </div>
       
       <div className="flex flex-col justify-center items-center">
-        <img src={banner} className="w-1/2" alt="banner" />
-        <p className="text-lg text-gray-700">Kyro Industries, the change the industry needs.</p>
-        <span className="text-sm text-gray-500 font-bold">No Bull Shit, Just Results</span>
+        <img 
+          src={banner} 
+          className="w-1/2 invert filter" 
+          alt="banner" 
+        />
+        <p className="text-lg text-gray-300">Kyro Industries, the change the industry needs.</p>
+        <span className="text-sm text-gray-400 font-bold">No Bull Shit, Just Results</span>
       </div>
       <div className="text-center mt-52">
-        <p className="text-sm text-gray-500">© {new Date().getFullYear()} Kyro. All rights reserved.</p>
+        <p className="text-sm text-gray-400">© {new Date().getFullYear()} Kyro. All rights reserved.</p>
       </div>
     </div>
   );
